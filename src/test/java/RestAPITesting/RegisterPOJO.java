753474a020378a180d9537b3e340f069b3fd60fc
Nikolina Mandicheva <nikolina.mandicheva@mentormate.com>
2022-06-13 01:28:32 +0300

    Selenium Tests - new package

---
+++ src/test/java/RestAPITesting/RegisterPOJO.java
@@ -0,0 +1,45 @@
+package RestAPITesting;
+
+public class RegisterPOJO {
+    private String username, email, password, dateBirth,publicInfo;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(String dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getPublicInfo() {
+        return publicInfo;
+    }
+
+    public void setPublicInfo(String publicInfo) {
+        this.publicInfo = publicInfo;
+    }
+}
