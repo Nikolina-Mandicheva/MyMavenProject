f44404d1f06c0698596ad702dd35be4355c1ea03
nikolina.mandicheva <nikolina.mandicheva@mentormate.com>
2022-06-13 08:33:43 +0300

    register pojo and method

---
+++ src/test/java/RestAPITesting/RegisterPOJO.java
@@ -0,0 +1,45 @@
+package RestAPITesting;
+
+public class RegisterPOJO {
+    private String username, email, birthDate, password, publicInfo;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPublicInfo() {
+        return publicInfo;
+    }
+
+    public void setPublicInfo(String publicInfo) {
+        this.publicInfo = publicInfo;
+    }
+}
